046375c3683e3a1d060eab0216ee97fe936190e6
PETER_GAO <pgao@berkeley.edu>
2015-02-24 09:55:02 -0800

    add imglab

---
+++ imglab/build/CMakeFiles/imglab.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/pgao/Downloads/dlib-18.13/tools/imglab/src/common.cpp" "/Users/pgao/Downloads/dlib-18.13/tools/imglab/build/CMakeFiles/imglab.dir/src/common.cpp.o"
+  "/Users/pgao/Downloads/dlib-18.13/tools/imglab/src/convert_idl.cpp" "/Users/pgao/Downloads/dlib-18.13/tools/imglab/build/CMakeFiles/imglab.dir/src/convert_idl.cpp.o"
+  "/Users/pgao/Downloads/dlib-18.13/tools/imglab/src/convert_pascal_v1.cpp" "/Users/pgao/Downloads/dlib-18.13/tools/imglab/build/CMakeFiles/imglab.dir/src/convert_pascal_v1.cpp.o"
+  "/Users/pgao/Downloads/dlib-18.13/tools/imglab/src/convert_pascal_xml.cpp" "/Users/pgao/Downloads/dlib-18.13/tools/imglab/build/CMakeFiles/imglab.dir/src/convert_pascal_xml.cpp.o"
+  "/Users/pgao/Downloads/dlib-18.13/tools/imglab/src/main.cpp" "/Users/pgao/Downloads/dlib-18.13/tools/imglab/build/CMakeFiles/imglab.dir/src/main.cpp.o"
+  "/Users/pgao/Downloads/dlib-18.13/tools/imglab/src/metadata_editor.cpp" "/Users/pgao/Downloads/dlib-18.13/tools/imglab/build/CMakeFiles/imglab.dir/src/metadata_editor.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/pgao/Downloads/dlib-18.13/tools/imglab/build/dlib_build/CMakeFiles/dlib.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/pgao/Downloads/dlib-18.13/dlib/.."
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
